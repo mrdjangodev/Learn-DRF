8e66164b507f61ee950df6147af3b82d67ce0ccb
mrdjangodev <mirolimcoder@gmail.com>
2023-05-02 15:59:11 +0300

    Todo tested by test.py and everything is going good

--- todo/config/settings.py
+++ todo/config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     
     'todos',
     'rest_framework',
+    'corsheaders',
     
     'django.contrib.admin',
     'django.contrib.auth',
@@ -52,12 +53,20 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# new
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+)
 
 ROOT_URLCONF = 'config.urls'
 
--- todo/todos/tests.py
+++ todo/todos/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+
+class TodoModelTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Todo.objects.create(title='first_todo', body='a body here', is_done=True)
+    
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEqual(expected_object_name, 'first_todo')
+        
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.body}'
+        self.assertEqual(expected_object_name, 'a body here')
+    
+    def test_is_done(self):
+        todo = Todo.objects.get(id=1)
+        excepted_object_is_done = todo.is_done
+        self.assertEqual(excepted_object_is_done, True)
