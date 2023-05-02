bffc6dd6f6cacb3d6c11bb055e1c82e64a829629
mrdjangodev <mirolimcoder@gmail.com>
2023-05-02 23:33:24 +0300

    Blog Post tested successfully

--- blog_api/posts/tests.py
+++ blog_api/posts/tests.py
@@ -1,1 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from .models import Post
+# Create your tests here.
+
+class BlogTests(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls) -> None:
+        # User is being created
+        test_user1 = User.objects.create(username="user1", password="qwert1234")
+        test_user1.save()
+        
+        # blog is being created
+        test_post1 = Post.objects.create(author=test_user1, title='Blog test title', body="Body is here")
+        test_post1.save()
+        
+    def testBlogContent(self):
+        post = Post.objects.get(id=1)
+        author = f"{post.author}"
+        title = f"{post.title}"
+        body = f"{post.body}"
+        self.assertEqual(author, "user1")
+        self.assertEqual(title, "Blog test title")
+        self.assertEqual(body, "Body is here")
+        
