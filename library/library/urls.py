b6bee427851da1ff0a2d59664312023a92a0fbbd
mrdjangodev <mirolimcoder@gmail.com>
2023-06-12 22:57:18 +0500

    swagger and redoc added to library project

--- library/library/urls.py
+++ library/library/urls.py
@@ -17,6 +17,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+# from rest_framework.schemas import get_schema_view   
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library Project",
+        description="Created for learning drf",
+        default_version='v1',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='mirolimcoder@gmail.com'),
+        license=openapi.License(name="License for Library Project"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
@@ -24,4 +42,9 @@ urlpatterns = [
     path('api-auth/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api-auth/dj-rest-auth/registrations/', include('dj_rest_auth.registration.urls')),
     path('api/allauth/', include('allauth.urls')),
+    
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-svagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc'),
 ]
