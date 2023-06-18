711e305ebbc852f1638505c7a8a03eebd0241b3c
mrdjangodev <mirolimcoder@gmail.com>
2023-06-18 22:29:16 +0500

    Markting models registered to admin panel

--- study_centr/marketing/admin.py
+++ study_centr/marketing/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import (
+    ServicePayment, Service, ServiceUsage, ServiceUser, SocialMedia, Interestor
+)
 # Register your models here.
+
+admin.site.register(Service)
+admin.site.register(ServicePayment)
+admin.site.register(ServiceUsage)
+admin.site.register(ServiceUser)
+admin.site.register(SocialMedia)
+admin.site.register(Interestor)
