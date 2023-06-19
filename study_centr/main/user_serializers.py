b158e424d2a2de754295147d47a2b3060b86e568
mrdjangodev <mirolimcoder@gmail.com>
2023-06-19 20:55:26 +0500

    visible user data limited

--- study_centr/main/user_serializers.py
+++ study_centr/main/user_serializers.py
@@ -10,4 +10,6 @@ class UserSerializer(serializers.ModelSerializer):
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'phone', 'created_at', 'updated_at')
+
+        
